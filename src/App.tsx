017d5370e4f21807e5cdd1eaea0a663231a67687
Mdgulab0786 <team66415@gmail.com>
2025-07-24 14:07:02 +0530

    Updated content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-
-import { Github, Linkedin, Mail, Instagram } from 'lucide-react';
+import { Github, Linkedin, Mail, Instagram } from "lucide-react";
 
 function App() {
   const socialLinks = [
-    { icon: Linkedin, href: 'https://www.linkedin.com/in/mojahid-ul-haque/',  label: 'LinkedIn' },
-    { icon: Github, href: 'https://github.com/mojahid0862', label: 'GitHub' },
-    { icon: Mail, href: 'mailto:hey@mojahidulhaque.com2', label: 'Email' },
-    { icon: Instagram, href: 'https://www.instagram.com/mojahid_ul_haque', label: 'Instagram' }
+    {
+      icon: Linkedin,
+      href: "https://www.linkedin.com/in/mojahid-ul-haque/",
+      label: "LinkedIn",
+    },
+    { icon: Github, href: "https://github.com/mojahid0862", label: "GitHub" },
+    { icon: Mail, href: "mailto:hey@mojahidulhaque.com2", label: "Email" },
+    {
+      icon: Instagram,
+      href: "https://www.instagram.com/mojahid_ul_haque",
+      label: "Instagram",
+    },
   ];
 
   return (
     <div className="min-h-screen bg-black text-white">
       <div className="container mx-auto px-4 py-12 max-w-md">
-      
-        
-      
         {/* Profile Section */}
         <div className="text-center mb-8">
           <div className="relative inline-block mb-6">
             <div className="w-24 h-24 mx-auto rounded-full overflow-hidden   shadow-lg">
-              <img 
-                src="aadil-bhai.png" 
+              <img
+                src="aadil-bhai.png"
                 alt="Profile"
                 className="w-full h-full object-cover"
               />
             </div>
           </div>
-          
+
           <h1 className="text-2xl font-bold mb-3">Mojahid-Ul Haque </h1>
-          <p className="text-gray-300 text-sm leading-relaxed mb-6 px-4">
-            DevOps Engineer | AWS, Azure & GCP | CI/CD, Docker, K8s, Ansible, Terraform | Automating, optimizing, problem-solving, Vibe coder.. Let's connect & build!
+          <p className="text-gray-300 fon text-sm leading-relaxed mb-6 px-4">
+            DevOps Engineer | AWS, Azure & GCP | CI/CD, Docker, K8s, Ansible,
+            Terraform | Automating, optimizing, problem-solving, Vibe coder..
+            Let's connect & build!
           </p>
 
           {/* Social Links */}
@@ -48,24 +54,21 @@ function App() {
           </div>
         </div>
 
-       
-
         {/* Portfolio Card */}
         <div className="mb-6">
           <a
             href="https://mojahidulhaque.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="block w-full bg-gray-900 hover:bg-gray-800 rounded-2xl border border-gray-800 hover:border-gray-700 transition-all duration-300 group overflow-hidden"
+            className="block w-full rounded-2xl border border-[#262626] hover:border-[#3d3d3d] transition-all duration-300 group overflow-hidden"
           >
-            <div className="p-4">
-              <img 
+            <div className="relative">
+              <img
                 src="portfolio.png"
                 alt="Portfolio"
-                className="w-full h-50 object-cover rounded-lg mb-3"
-              
+                className="w-full h-62 object-cover"
               />
-              <h3 className="font-medium text-white group-hover:text-yellow-400 transition-colors duration-300 text-center">
+              <h3 className="absolute bottom-3 left-2 text-lg font-semibold text-white  px-3 py-1 rounded">
                 Mojahid Ul Haque - Portfolio
               </h3>
             </div>
@@ -79,34 +82,33 @@ function App() {
             href="https://ip.crafzo.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="block w-full bg-gray-900 hover:bg-gray-800 rounded-2xl border border-gray-800 hover:border-gray-700 transition-all duration-300 group overflow-hidden"
+            className="block w-full rounded-2xl border border-[#262626] hover:border-[#3d3d3d] transition-all duration-300 group overflow-hidden"
           >
-            <div className="p-4">
-              <img 
+            <div className="relative">
+              <img
                 src="/IP-Geolocation Lookup.png"
                 alt="IP Lookup by Crafzo"
-                className="w-full h-50 object-cover rounded-lg mb-3"
+                className="w-full h-62 object-cover"
               />
-              <h3 className="font-medium text-white group-hover:text-yellow-400 transition-colors duration-300 text-center">
+              <h3 className="absolute bottom-3 left-4 text-lg font-semibold text-white  px-3 py-1 rounded">
                 IP Lookup by Crafzo
               </h3>
             </div>
           </a>
-
           {/* Fridge Feast by Crafzo */}
           <a
             href="https://crafzo.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="block w-full bg-gray-900 hover:bg-gray-800 rounded-2xl border border-gray-800 hover:border-gray-700 transition-all duration-300 group overflow-hidden"
+            className="block w-full rounded-2xl border border-[#262626] hover:border-[#3d3d3d] transition-all duration-300 group overflow-hidden"
           >
-            <div className="p-4">
-              <img 
+            <div className="relative">
+              <img
                 src="Fridge-Feast.png"
                 alt="Fridge Feast by Crafzo"
-                className="w-full h-50 object-cover rounded-lg mb-3"
+                className="w-full h-62 object-cover"
               />
-              <h3 className="font-medium text-white group-hover:text-yellow-400 transition-colors duration-300 text-center">
+              <h3 className="absolute bottom-3 left-4 text-lg font-semibold text-white  px-3 py-1 rounded">
                 Fridge Feast by Crafzo
               </h3>
             </div>
@@ -117,34 +119,34 @@ function App() {
             href="https://ccgen.xyz/"
             target="_blank"
             rel="noopener noreferrer"
-            className="block w-full bg-gray-900 hover:bg-gray-800 rounded-2xl border border-gray-800 hover:border-gray-700 transition-all duration-300 group overflow-hidden"
+            className="block w-full rounded-2xl border border-[#262626] hover:border-[#3d3d3d] transition-all duration-300 group overflow-hidden"
           >
-            <div className="p-4">
-              <img 
+            <div className="relative">
+              <img
                 src="CC-Generator.png"
                 alt="Credit Card Generator"
-                className="w-full h-50 object-cover rounded-lg mb-3"
+                className="w-full h-62 object-cover"
               />
-              <h3 className="font-medium text-white group-hover:text-yellow-400 transition-colors duration-300 text-center">
+              <h3 className="absolute bottom-3 left-4 text-lg font-semibold text-white  px-3 py-1 rounded">
                 Credit Card Generator for Testing
               </h3>
             </div>
           </a>
 
-              {/* DNS & SSL Toolkit*/}
+          {/* DNS & SSL Toolkit */}
           <a
             href="https://dns.crafzo.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="block w-full bg-gray-900 hover:bg-gray-800 rounded-2xl border border-gray-800 hover:border-gray-700 transition-all duration-300 group overflow-hidden"
+            className="block w-full rounded-2xl border border-[#262626] hover:border-[#3d3d3d] transition-all duration-300 group overflow-hidden"
           >
-            <div className="p-4">
-              <img 
+            <div className="relative">
+              <img
                 src="DNS-&-SSL-Toolkit.png"
-                alt="Credit Card Generator"
-                className="w-full h-50 object-cover rounded-lg mb-3"
+                alt="DNS & SSL Toolkit"
+                className="w-full h-62 object-cover"
               />
-              <h3 className="font-medium text-white group-hover:text-yellow-400 transition-colors duration-300 text-center">
+              <h3 className="absolute bottom-3 left-4 text-lg font-semibold text-white  px-3 py-1 rounded">
                 DNS & SSL Toolkit
               </h3>
             </div>
@@ -155,22 +157,20 @@ function App() {
             href="https://www.linkedin.com/newsletters/new-school-devops-7287344762305552385/"
             target="_blank"
             rel="noopener noreferrer"
-            className="block w-full bg-gray-900 hover:bg-gray-800 rounded-2xl border border-gray-800 hover:border-gray-700 transition-all duration-300 group overflow-hidden"
+            className="block w-full rounded-2xl border border-[#262626] hover:border-[#3d3d3d] transition-all duration-300 group overflow-hidden"
           >
-            <div className="p-4">
-              <img 
+            <div className="relative">
+              <img
                 src="New School DevOps.png"
                 alt="New School DevOps"
-                className="w-full h-51 object-cover rounded-lg mb-3"
+                className="w-full h-62 object-cover"
               />
-              <h3 className="font-medium text-white group-hover:text-yellow-400 transition-colors duration-300 text-center">
+              <h3 className="absolute bottom-3 left-4 text-lg font-semibold text-white  px-3 py-1 rounded">
                 New School DevOps. | LinkedIn
               </h3>
             </div>
           </a>
         </div>
-
-        
 
         {/* Footer */}
         <footer className="text-center mt-12 pt-8 border-t border-gray-800">
@@ -178,7 +178,6 @@ function App() {
             © 2025 Mojahid-Ul Haque. All rights reserved.
           </p>
         </footer>
-
       </div>
     </div>
   );
